d1df6a8357aed77e72368b3708365ed85b59fac4
Daniel Schulz <faulolio@gmail.com>
2021-09-28 15:31:25 +0200

    Format code

--- Assets/MeshGeneration/Scripts/Vertex.cs
+++ Assets/MeshGeneration/Scripts/Vertex.cs
@@ -1,40 +1,43 @@
 using UnityEngine;
 
-// Pseudoimplementierung - nicht die tatsächliche API
-public struct PointMesh {
-    Vertex[] vertices;
-    Vector1Int[] points;
-}
-public struct LineMesh {
-    Vertex[] vertices;
-    Vector2Int[] lines;
-}
-public struct TriangleMesh {
-    Vertex[] vertices;
-    Vector3Int[] triangles;
-}
-public struct QuadMesh {
-    Vertex[] vertices;
-    Vector4Int[] quads;
-}
+namespace Pseudocode {
+    // Pseudoimplementierung - nicht die tatsächliche API
+    public struct PointMesh {
+        Vertex[] vertices;
+        Vector1Int[] points;
+    }
+    public struct LineMesh {
+        Vertex[] vertices;
+        Vector2Int[] lines;
+    }
+    public struct TriangleMesh {
+        Vertex[] vertices;
+        Vector3Int[] triangles;
+    }
+    public struct QuadMesh {
+        Vertex[] vertices;
+        Vector4Int[] quads;
+    }
 
+    // Pseudoimplementierung - nicht die tatsächliche API
+    public struct Vertex {
+        public Vector3 position;
+        public Vector3 normal;
+        public Vector3 tangent;
+    }
 
+    public struct Vector1Int {
+    }
+    public struct Vector4Int {
+    }
 
-// Pseudoimplementierung - nicht die tatsächliche API
-public struct Vertex {
-    public Vector3 position;
-    public Vector3 normal;
-}
+    public class Mesh {
+        Vector3[] vertices;
+        Vector3[] normals;
+        Vector3[] tangents;
+        int[] triangles;
 
-public struct Vector1Int {
-}
-public struct Vector4Int {
-}
-
-
-
-public class Mesh {
-    Vector3[] vertices;
-    Vector3[] normals;
-    int[] triangles;
+        public void RecalculateNormals() { }
+        public void RecalculateTangents() { }
+    }
 }
