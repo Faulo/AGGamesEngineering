a6595ca0f8f2d32e55f9506aa6f6b5d7ac1c0ec4
Katharina Broswik <Katharina_B@live.de>
2021-12-06 18:50:56 +0100

    Add tab functionality

---
+++ Assets/UI/Scripts/TabButton.cs
@@ -0,0 +1,57 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler {
+    [HideInInspector]
+    public Image background = default;
+
+    [SerializeField]
+    TabGroup tabGroup = default;
+    [SerializeField]
+    public TextMeshProUGUI text = default;
+
+    [SerializeField]
+    UnityEvent onTabSelected = default;
+    [SerializeField]
+    UnityEvent onTabDeselected = default;
+
+    protected void Awake() {
+        SetUpComponents();
+    }
+    protected void Start() {
+        SetUpComponents();
+    }
+    protected void SetUpComponents() {
+        if (!background) {
+            TryGetComponent(out background);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        tabGroup.OnTabExit(this);
+    }
+
+    public void Select() {
+        if (onTabSelected != null) {
+            onTabSelected.Invoke();
+        }
+    }
+
+    public void Deselect() {
+        if (onTabDeselected != null) {
+            onTabDeselected.Invoke();
+        }
+    }
+}
