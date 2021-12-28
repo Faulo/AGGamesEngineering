312c6b9eb3bfc4d2cf28e81ba12e9a63e2eb5147
Daniel Schulz <faulolio@gmail.com>
2021-12-28 15:36:15 +0100

    Format code

--- Assets/UI/Scripts/Item.cs
+++ Assets/UI/Scripts/Item.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace AGGE.UI {
     public class Item : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler {
@@ -14,7 +14,7 @@ namespace AGGE.UI {
             itemImage = GetComponent<Image>();
             itemRectTransfrom = GetComponent<RectTransform>();
             lastSlot = GetComponentInParent<InventorySlot>();
-            if(transform.root.gameObject.TryGetComponent(out Canvas canvas)) {
+            if (transform.root.gameObject.TryGetComponent(out Canvas canvas)) {
                 mainCanvas = canvas;
             }
         }
@@ -31,14 +31,13 @@ namespace AGGE.UI {
         public void OnPointerUp(PointerEventData eventData) {
             itemImage.raycastTarget = true;
 
-            if (eventData.pointerCurrentRaycast.gameObject.TryGetComponent(out InventorySlot slot)) {   
+            if (eventData.pointerCurrentRaycast.gameObject.TryGetComponent(out InventorySlot slot)) {
                 transform.SetParent(slot.transform);
                 transform.localPosition = Vector3.zero;
                 slot.SetItem(this);
                 lastSlot.SetItem(null);
                 lastSlot = slot;
-            } 
-            else {
+            } else {
                 transform.SetParent(lastSlot.transform);
                 transform.localPosition = Vector3.zero;
             }
